516a05ee045cd67b61c53c226f62364ae544ed89
YoungLad <gabrieljohnsoniv@gmail.com>
2016-08-19 02:06:36 -0400

    finished chapter 2

--- Ex_Files_LReact_js_2016/Exercise Files/Ch03/03_04/start/js/Note.js
+++ Ex_Files_LReact_js_2016/Exercise Files/Ch03/03_04/start/js/Note.js
@@ -1,6 +1,9 @@
 var Note = React.createClass({
     render: function() {
-        
+    	return (
+    		<div className="note"><div>
+    		);
+        }
 });
 
 React.render(<Note>Hello World</Note>, 
